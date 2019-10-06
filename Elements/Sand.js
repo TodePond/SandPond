5c3cee979a16bdaedccaf4fab877d3ce7bd8095c
Luke Wilson <l2wilson94@gmail.com>
2019-10-06 19:03:32 +0100

    Update Sand.js

--- Elements/Sand.js
+++ Elements/Sand.js
@@ -1,11 +1,13 @@
-TodeSPLAT `element Sand {
-		colour #ffcc00
-		emissive #ffa34d
-		
-		state solid
-		
-		rule fallRule
-		rule slideRule
-	}
+TodeSPLAT `
+
+element Sand {
+	colour #ffcc00
+	emissive #ffa34d
+	
+	state solid
+	
+	rule fallRule
+	rule slideRule
+}
 	
 `
