a00fb65611d45118e3ce407c92a002e8be74127b
Luke Wilson <l2wilson94@gmail.com>
2019-10-11 20:46:47 +0100

    Oops I forgot Water

--- Elements/Water.js
+++ Elements/Water.js
@@ -1,19 +1,11 @@
 TodeSplat`
 
-
 element Water {
 
 	colour "lightblue"
 	emissive "blue"
 	
 	state "liquid"
-
-	input @ (space, args) => args.self = space.atom
-	input _ (space) => space && space.atom == undefined
-	input # (space) => space && space.atom != undefined
-	
-	output _ (space) => setSpaceAtom(space, undefined)
-	output @ (space, {self}) => setSpaceAtom(space, self)
 	
 	rule {
 		@ => _
@@ -26,8 +18,5 @@ element Water {
 	}
 	
 }
-
-
-
 
 `
