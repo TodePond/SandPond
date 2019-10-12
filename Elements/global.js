e1e4f527797d1148b12615f35bb3d6808177f57e
Luke Wilson <l2wilson94@gmail.com>
2019-10-12 16:00:54 +0100

    Added "x" global input
    for non-existant space (ie: the edge of the universe)

--- Elements/global.js
+++ Elements/global.js
@@ -1,14 +1,13 @@
 TodeSplat`
 
-
-
 input @ (space, args) => args.self = space.atom
 input _ (space) => space && !space.atom
 input # (space) => space && space.atom
 input . (space) => space
+input x (space) => !space
 
 output @ (space, {self}) => setSpaceAtom(space, self)
 output _ (space) => setSpaceAtom(space, undefined)
-
+output . () => {}
 
 `
