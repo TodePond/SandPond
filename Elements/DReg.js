9aaecf96dd9bf55110d3b3e36a39cbea61226f3d
Luke Wilson <l2wilson94@gmail.com>
2019-10-16 20:54:45 +0100

    Added aspirational code to DReg

--- Elements/DReg.js
+++ Elements/DReg.js
@@ -12,12 +12,28 @@ element DReg {
 	output R ({space}) => setSpaceAtom(space, makeAtom(Res))
 	output D ({space}) => setSpaceAtom(space, makeAtom(DReg))
 	
-	
 	rule xyz 0.001 { @_ => D@ }
 	rule xyz 0.005 { @_ => R@ }
 	rule xyz 0.1 { @D => _@ }
 	rule xyz 0.01 { @n => _@ }
 	rule xyz { @_ => _@ }
+	
+	// Aspirational code
+	/*
+	rule xyz {
+		@_ => {
+			rule xyz 0.001 { @_ => @D }
+			rule xyz 0.005 { @_ => @R }
+		}
+	}
+	
+	rule xyz {
+		@# => {
+			rule xyz 0.1 { @D => @_ }
+			rule xyz 0.1 { @n => @_ }
+		}
+	}
+	*/
 	
 }
 
