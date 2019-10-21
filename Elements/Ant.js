2e74ddb85ca41ac1bfed387ee1d4d8bc0ecb0729
Luke Wilson <l2wilson94@gmail.com>
2019-10-21 22:02:48 +0100

    Ants starve

--- Elements/Ant.js
+++ Elements/Ant.js
@@ -6,13 +6,16 @@ element Ant {
 	colour "grey"
 	emissive "black"
 	precise true
-		
+	
+	input F ({space}) => space && space.atom && space.atom.type == Food
+	
+	
+	rule 0.001 { @ => _ }
+	
 	rule {
 		@ => _
 		_    @
 	}
-	
-	input F ({space}) => space && space.atom && space.atom.type == Food
 	
 	rule xyz 0.02 { @F => @@ }
 	rule xyz { @F => _@ }
@@ -22,6 +25,8 @@ element Ant {
 		 _ =>  @
 		@#    _#
 	}
+	
+	
 		
 	
 }
