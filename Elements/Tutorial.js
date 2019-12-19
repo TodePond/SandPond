e6179a67043be936b9a7aa0e0ce94df1b134eb64
Luke Wilson <l2wilson94@gmail.com>
2019-12-19 19:45:45 +0000

    Create Tutorial.js

---
+++ Elements/Tutorial.js
@@ -0,0 +1,53 @@
+TodeSplat`
+
+element GreenSand {
+
+	colour "green"
+	category "Tutorial"
+	default true
+
+	given W (element) => element == PinkWater
+	change W () => new PinkWater()
+	
+	rule {
+		@ => W
+		W    @
+	}
+	
+	rule xz {
+		@  => W
+		#W    #@
+	}
+	
+	rule {
+		@ => _
+		_    @
+	}
+	
+	rule xz {
+		@  => _
+		#_    #@
+	}
+	
+}
+
+element PinkWater {
+
+	colour "pink"
+	emissive "purple"
+	opacity 0.5
+	category "Tutorial"
+	
+	rule {
+		@ => _
+		_    @
+	}
+	
+	rule xz {
+		@_ => _@
+		#     #
+	}
+	
+}
+
+`
