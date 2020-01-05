fe68fcb6a2c6de1d35e21581d3c597fb13747094
Luke Wilson <l2wilson94@gmail.com>
2020-01-05 11:52:21 +0000

    add Herbivore and Plant
    - and updated herbivore to TodeSplat 2

--- Elements/Life.js
+++ Elements/Life.js
@@ -107,4 +107,38 @@ element Cycler {
 	
 }
 
+element Plant {
+	colour "green"
+	category "Life"
+	
+	// Gravity
+	ruleset Powder
+	
+	// Grow
+	rule xz 0.05 { @_ => @@ }
+	
+}
+
+element Herbivore {
+	colour "blue"
+	emissive "darkblue"
+	category "Life"
+	
+	// Die
+	rule 0.002 { @ => _ }
+	
+	// Gravity
+	ruleset Powder
+	
+	// Reproduce
+	given P (element) => element == Plant
+	rule xyz 0.05 { @P => @@ }
+	
+	// Eat
+	rule xyz { @P => @_ }
+	
+	// Move
+	rule xz 0.5 { @_ => _@ }
+}
+
 `
