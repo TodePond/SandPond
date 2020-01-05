1c968a53678ec2782d1a52311d4ad8d37848a309
Luke Wilson <l2wilson94@gmail.com>
2020-01-05 13:39:47 +0000

    coloured sparks and explosions

--- Elements/Explosive.js
+++ Elements/Explosive.js
@@ -6,7 +6,7 @@ element Spark {
 	emissive "orange"
 	category "Explosive"
 	
-	given s (element) => element == Spark
+	given s (element, self) => element == self.element
 	keep s
 	
 	rule 0.2 { @ => _}
@@ -16,23 +16,56 @@ element Spark {
 	
 }
 
+element BlueSpark {
+
+	colour "lightblue"
+	emissive "lightblue"
+	category "Explosive"
+	
+	ruleset Spark
+	
+}
+
 element Explosion {
 
 	colour "lightyellow"
 	emissive "orange"
 	category "Explosive"
-	default true
 	
 	data timer 20
 	
 	given t (self) => self.timer--
 	given d (self) => self.timer <= 0
-	change E (self) => new Explosion({timer: self.timer})
+	change E (self) => new self.element({timer: self.timer})
 	
 	action { @t => .. }
 	rule { @d => _. }
 	
 	rule xyz { @. => @E }
+	
+}
+
+element RedExplosion {
+
+	colour "red"
+	emissive "darkred"
+	category "Explosive"
+	
+	data timer 20
+	
+	ruleset Explosion
+	
+}
+
+element BlueExplosion {
+
+	colour "blue"
+	emissive "darkblue"
+	category "Explosive"
+	
+	data timer 20
+	
+	ruleset Explosion
 	
 }
 
