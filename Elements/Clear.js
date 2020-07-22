bd625b424e6924204dab207fe0ea0afc7676a125
Luke Wilson <l2wilson94@gmail.com>
2020-07-22 18:07:00 +0100

    Clear!

--- Elements/Clear.js
+++ Elements/Clear.js
@@ -1,5 +1,60 @@
 SpaceTode`
 
+element Clear2D {
+	colour "brown"
+	category "Clear"
+	
+	symbol C Clear2D
+	symbol D Clear2D.Done
+	symbol B Clear2D.Bomb
+	
+	given O (element) => element !== Void && element !== Clear2D && element !== Clear2D.Done
+	
+	
+	all(xy) {
+		@O => .C
+	}
+	
+	any(xy) {
+		@x => D.
+		@D => D.
+	}
+	
+	element Done {
+		colour "blue"
+		all(xy) @C => ..
+		@ => _
+	}
+	
+}
 
+element Clear3D {
+	colour "brown"
+	category "Clear"
+	
+	symbol C Clear3D
+	symbol D Clear3D.Done
+	symbol B Clear3D.Bomb
+	
+	given O (element) => element !== Void && element !== Clear3D && element !== Clear3D.Done
+	
+	
+	all(xyz.rotations) {
+		@O => .C
+	}
+	
+	any(xyz.rotations) {
+		@x => D.
+		@D => D.
+	}
+	
+	element Done {
+		colour "blue"
+		emissive "darkblue"
+		all(xyz.rotations) @C => ..
+		@ => _
+	}
+	
+}
 
 `
