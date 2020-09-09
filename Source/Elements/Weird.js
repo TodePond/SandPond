7878caa5c7c377b55ccf1def3cf60fc660c2bcc1
Luke Wilson <l2wilson94@gmail.com>
2020-09-09 20:01:20 +0100

    Glooper

---
+++ Source/Elements/Weird.js
@@ -0,0 +1,28 @@
+SpaceTode`
+
+element Glooper {
+	
+	colour "rgb(10, 120, 200)"
+	category "Weird"
+	
+	
+	maybe(0.2) {
+		@ => _
+		_    @
+	}	
+	
+	for(xyz.directions) {
+		@_$ => _@.
+	}
+	
+	for(xyz.directions) {
+		__@_ => .._@
+	}
+	
+	
+	
+}
+
+
+`
+
