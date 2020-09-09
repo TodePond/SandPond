dae08d45044556fba06dd6496ac98c97a9ecb672
Luke Wilson <l2wilson94@gmail.com>
2020-09-09 20:01:59 +0100

    Gloopoe

--- Source/Elements/Weird.js
+++ Source/Elements/Weird.js
@@ -5,6 +5,7 @@ element Glooper {
 	colour "rgb(10, 120, 200)"
 	category "Weird"
 	
+	symbol g Gloopoe
 	
 	maybe(0.2) {
 		@ => _
@@ -19,7 +20,32 @@ element Glooper {
 		__@_ => .._@
 	}
 	
+	maybe(0.001) @ => g
 	
+	
+	
+}
+	
+element Gloopoe {
+	
+	colour "rgb(150, 10, 80)"
+	category "Weird"
+	
+	symbol g Glooper
+	
+	maybe(0.2) {
+		_ => @
+		@    _
+	}	
+	
+	for(xyz.directions) {
+		@_$ => _@.
+	}
+	
+	for(xyz.directions) {
+		__@_ => .._@
+	}
+	maybe(0.001) @ => g
 	
 }
 
