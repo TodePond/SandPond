366f3c16e45520459a995ebc92d174fda89e2fe8
Luke Wilson <l2wilson94@gmail.com>
2020-09-10 20:38:29 +0100

    up and down signal

--- Source/Elements/Weird.js
+++ Source/Elements/Weird.js
@@ -1,10 +1,37 @@
 SpaceTode`
 
 element Glooper {
-	default true
+
 	colour "rgb(110, 120, 200)"
 	category "Weird"
 	
+	arg direction "down"
+	arg directionTime 0
+	
+	keep c (self) => {
+		if (self.direction == "down") {
+			self.colour.r = 110
+			self.colour.g = 120
+			self.colour.b = 200
+		}
+		else {
+			self.colour.r = 110
+			self.colour.g = 200
+			self.colour.b = 1
+		}
+	}
+	action @ => c
+	
+	given D (Self, element, self, atom) => {
+		return Self === element && self.directionTime < atom.directionTime
+	}
+	keep D (self, atom) => {
+		self.direction = atom.direction
+		self.directionTime = atom.directionTime
+	}
+	for(xyz.directions) {
+		@D => .D
+	}
 	
 	for(xyz.directions) {
 		@_$ => _@.
@@ -15,19 +42,105 @@ element Glooper {
 		_$    @.
 	}
 	
+	given d (self, element) => element === Empty && self.direction == "down"
 	maybe(0.2) {
 		@ => _
-		_    @
+		d    @
+	}
+	
+	given u (self, element) => element === Empty && self.direction == "up"
+	maybe(0.2) {
+		u => @
+		@    _
 	}
 	
 	any(xz.directions) {
 		@  => _
-		$_    .@
-	}
+		$d    .@
 		
+		$u    .@
+		@  => _
+	}
+	
 	
 	
 }
+
+element AntiGlooper {
+	
+	colour "rgb(200, 120, 110)"
+	category "Weird"
+	
+	symbol G Glooper
+	symbol E Eater
+	
+	for(xyz.directions) {
+		@G => EE
+	}
+	
+	mimic(Glooper)
+	
+}
+
+element Gridder {
+	
+	colour "rgb(50, 150, 50)"
+	category "Weird"
+	
+	arg timer 300
+	symbol S Glooper
+	
+	given t (self) => self.timer-- <= 0
+	t => S
+	
+	change G (self, Self) => new Self(self.timer)
+	
+	any(xyz.directions) {
+		maybe(0.1) @_ => @G
+		@$ => _@
+		@_ => _@
+	}
+	
+}
+
+element UpSignal {
+	
+	symbol G Glooper
+	keep u (atom, time) => {
+		atom.direction = "up"
+		atom.directionTime = time
+	}
+	category "Weird"
+	
+	@ => _
+	_    @
+	
+	@ => _
+	G    u
+	
+	@ => _
+}
+
+
+element DownSignal {
+	
+	symbol G Glooper
+	keep d (atom, time) => {
+		atom.direction = "down"
+		atom.directionTime = time
+	}
+	category "Weird"
+	
+	@ => _
+	_    @
+	
+	
+	@ => _
+	G    d
+	
+	@ => _
+}
+
 
 
 `
