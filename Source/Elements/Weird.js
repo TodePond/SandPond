9a9207c8a830193a580f3b7a75f2adb7ee06e3f5
Luke Wilson <l2wilson94@gmail.com>
2020-09-10 19:56:08 +0100

    Gloopergloop

--- Source/Elements/Weird.js
+++ Source/Elements/Weird.js
@@ -1,51 +1,31 @@
 SpaceTode`
 
 element Glooper {
-	
-	colour "rgb(10, 120, 200)"
+	default true
+	colour "rgb(110, 120, 200)"
 	category "Weird"
 	
-	symbol g Gloopoe
+	
+	for(xyz.directions) {
+		@_$ => _@.
+	}
+	
+	for(xyz.rotations) {
+		@  => _
+		_$    @.
+	}
 	
 	maybe(0.2) {
 		@ => _
 		_    @
-	}	
-	
-	for(xyz.directions) {
-		@_$ => _@.
 	}
 	
-	for(xyz.directions) {
-		__@_ => .._@
+	any(xz.directions) {
+		@  => _
+		$_    .@
 	}
+		
 	
-	maybe(0.001) @ => g
-	
-	
-	
-}
-	
-element Gloopoe {
-	
-	colour "rgb(150, 10, 80)"
-	category "Weird"
-	
-	symbol g Glooper
-	
-	maybe(0.2) {
-		_ => @
-		@    _
-	}	
-	
-	for(xyz.directions) {
-		@_$ => _@.
-	}
-	
-	for(xyz.directions) {
-		__@_ => .._@
-	}
-	maybe(0.001) @ => g
 	
 }
 
