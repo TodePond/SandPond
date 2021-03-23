7618ed68dcc499a0ad9729cfbf0e4c61a605c89a
Luke Wilson <l2wilson94@gmail.com>
2021-03-23 18:24:12 +0000

    rainglow sand

--- Source/Elements/Weird.js
+++ Source/Elements/Weird.js
@@ -27,6 +27,48 @@ element RainbowSand {
 	}
 }
 
+element RainglowSand {
+	category "Video"
+	
+	prop colours {
+		const arr = []
+		for (let i = 0; i < 360; i++) {
+			const colour = new THREE.Color("hsl(" + i + ", 100%, 50%)")
+			const cs = [colour.r, colour.g, colour.b].map(c => Math.floor(c * 255))
+			arr.push(cs)
+			
+		}
+		return arr
+	}
+	data ready false
+	data offset 0
+	keep i (space, self, time, Self) => {
+		if (!self.ready) {
+			self.offset = time
+			self.ready = true
+		}
+		const [r, g, b] = Self.colours[(time + self.offset) % 360]
+		self.colour.r = r
+		self.colour.g = g
+		self.colour.b = b
+		self.emissive.r = r
+		self.emissive.g = g
+		self.emissive.b = b
+		SPACE.update(space)
+	}
+	action @ => i
+	
+	any(xz.directions) {
+		@ => _
+		_    @
+		
+		@  => _
+		 _     @
+		
+		@ => @
+	}
+}
+
 element Glooper {
 
 	colour "rgb(110, 120, 200)"
